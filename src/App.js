fadc81c3f73d7a340c88b0a513a78b4c83bb22dc
Ibrahima Capo-Chichi <40813947+icapochi@users.noreply.github.com>
2022-12-19 17:52:39 -0500

    Set up router and installed supabase + chakraui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Lost from './pages/Lost'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='*' element={<Lost />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Lost.js
@@ -0,0 +1,12 @@
+
+
+export default function Home(){
+
+
+
+    return(
+        <>
+            <h1>Bad Url</h1>
+        </>
+    )
+}

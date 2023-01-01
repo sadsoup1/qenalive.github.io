3eef46ad881f0f4073482a9c2417422eb493d162
John <john@ruffell>
2023-01-01 16:18:50 -0500

    Added side bit for main content responsive to nav

---
+++ src/pages/components/Interface.js
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Interface() {
+    return (
+        <Box w='100vw' h='100vh' bg='gray.100'>
+            
+        </Box>
+    )
+}

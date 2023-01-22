c7c8c56f2f0c0a915f6a6b165994450b9868c1a5
John Ruffell <john@ruffell.com>
2023-01-21 19:13:52 -0500

    Revert "Fixed content width"
    This reverts commit f82cc316f4760ad770f122aea4781d49c586a233.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { Box, Heading } from "@chakra-ui/react"
 
 export default function Home(){
 return (
-        <Box w='100vw' h='100vh' bg='gray.100'>
+        <Box bg='blue'>
             <Heading pos='fixed' top='50vh' right='50vw'>
                 Home
             </Heading>

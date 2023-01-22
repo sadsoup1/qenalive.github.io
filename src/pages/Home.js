5f936b4ad91b6db31f60c397f770c4dbe71c2d58
John Ruffell <john@ruffell.com>
2023-01-22 14:54:48 -0500

    added modal for room creation and supabase for it

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
-import { Box, Button, Center, Flex, Input, InputGroup, InputLeftElement, SimpleGrid } from "@chakra-ui/react"
+import { Box, Button, Center, Flex, FormControl, FormLabel, Input, InputGroup, InputLeftElement, SimpleGrid, useDisclosure } from "@chakra-ui/react"
 import { IoIosSearch } from 'react-icons/io'
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from '@chakra-ui/react'
+import supabase from '../supabase'
+import { useState } from 'react'
+
 
 export default function Home() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [id, setRoomID ] = useState('')
+    const [name, setRoomName ] = useState('')
+
+    const CreateRoom = async function () {
+        const { data, error } = await supabase
+        .from('rooms')
+        .insert([
+            { id, name },
+        ])
+
+        onClose()
+    }
+
     return (
         <Box w='100vw' h='100vh' bg='gray.100' overflow={'scroll'}>
             <SimpleGrid 
@@ -29,9 +47,11 @@ export default function Home() {
                             </InputGroup>
                         </Flex>
                         <Flex>
-                            <Button border='1px' borderColor='gray.200' m={4} ml='14'>
+                            <Button border='1px' borderColor='gray.200' m={4} ml='14' onClick={onOpen} >
                                 Create a Room
                             </Button>
+
+                            
                         </Flex>
                         
                     </Flex>
@@ -47,6 +67,29 @@ export default function Home() {
                     </Box>
                 </Center>
             </SimpleGrid>
+            
+            <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                <ModalOverlay />
+                <ModalContent>
+                <ModalHeader>Create a new room</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <FormControl isRequired>
+                        <FormLabel>Room ID</FormLabel>
+                        <Input onChange={(e)=>{setRoomID(e.target.value)}} placeholder='ABCD-1234' />
+                        <FormLabel mt={10}>Room Name</FormLabel>
+                        <Input mb={5} onChange={(e)=>{setRoomName(e.target.value)}} placeholder='Class Name - Section Name' />
+                    </FormControl>
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button mr={3} onClick={CreateRoom}>Create room</Button>
+                    <Button mr={3} onClick={onClose}>
+                    Close
+                    </Button>
+                </ModalFooter>
+                </ModalContent>
+            </Modal>
         </Box>
     )
 }

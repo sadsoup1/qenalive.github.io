f82cc316f4760ad770f122aea4781d49c586a233
John Ruffell <john@ruffell.com>
2023-01-21 19:12:56 -0500

    Fixed content width

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { Box, Heading } from "@chakra-ui/react"
 
 export default function Home(){
 return (
-        <Box bg='blue'>
+        <Box w='100vw' h='100vh' bg='gray.100'>
             <Heading pos='fixed' top='50vh' right='50vw'>
                 Home
             </Heading>

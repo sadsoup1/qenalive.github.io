1ffff06b98d70a205af5d60f860dc7042688772e
John Ruffell <john@ruffell.com>
2023-01-21 19:13:26 -0500

    Fixed main content area to flex to sidebar

--- src/App.js
+++ src/App.js
@@ -7,29 +7,32 @@ import Settings from './pages/Settings'
 import Classes from './pages/Classes';
 import SideNav from './pages/components/SideNav';
 import RequireAuth from './pages/components/RequireAuth';
+import { Flex } from '@chakra-ui/react';
 
 function App() {
   return (
     <>
-      {/* Side Nav stacked ontop of everything but hidden without Auth */}
-      <SideNav />
-    
-      <Routes>
-        {/* Before User Sign In */}
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Login />} />
+      <Flex>
+        {/* Side Nav stacked ontop of everything but hidden without Auth */}
+        <SideNav />
+      
+        <Routes>
+          {/* Before User Sign In */}
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Login />} />
 
-        {/* 
-          After User Sign In 
-          Wrapped in Auth component to restrict to user login
-        */}
-        <Route path="/" element={<RequireAuth> <Home /> </RequireAuth>} />
-        <Route path='/classes' element={<RequireAuth> <Classes /> </RequireAuth>} />
-        <Route path='/settings' element={<RequireAuth> <Settings /> </RequireAuth>}/>
-        
-        {/* Other */}
-        <Route path='*' element={<Lost />} />
-      </Routes>
+          {/* 
+            After User Sign In 
+            Wrapped in Auth component to restrict to user login
+          */}
+          <Route path="/" element={<RequireAuth> <Home /> </RequireAuth>} />
+          <Route path='/classes' element={<RequireAuth> <Classes /> </RequireAuth>} />
+          <Route path='/settings' element={<RequireAuth> <Settings /> </RequireAuth>}/>
+          
+          {/* Other */}
+          <Route path='*' element={<Lost />} />
+        </Routes>
+      </Flex>
     </>
   );
 }
